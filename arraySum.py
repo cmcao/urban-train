b885d3afd2f39028d16f9a6c27ebe405d251a7f6
cmcao <43836574+cmcao@users.noreply.github.com>
2018-10-31 00:05:40 -0700

    Add files via upload

---
+++ arraySum.py
@@ -0,0 +1,19 @@
+'''
+This call will give the sum of the array in
+'''
+import sys
+
+def array_sum(array_num):
+    sum = 0
+    for val_i in array_num:
+        sum += val_i
+    return sum
+
+list = [3,4,5,6,7]
+print("The list contain:",list)
+
+print("Total sum of list is:",array_sum(list))
+
+print(sys.version)
+
+print("hello world")
